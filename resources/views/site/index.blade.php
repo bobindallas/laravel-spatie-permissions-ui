bed594564aaf2e39e231bb42651239be9f7bee88
bob <bob@sivercrowlabsl.com>
2019-11-22 11:48:20 -0600

    updates

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -9,5 +9,5 @@
 	<p class="lead">Login above:</p>
 	<p class="lead">Superuser: super@email.com (has all privileges)</p>
 	<p class="lead">Admin: admin@email.com</p>
-	<p class="lead">User: user@emai.com</p>
+	<p class="lead">User: user@email.com</p>
 @stop

584a9694ad6c4d55f55a68879ac3aa06e7692520
bob <bob@sivercrowlabsl.com>
2019-11-29 17:39:15 -0600

    updates

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 	<h1>Roles &amp; Permissions UI...</h1>
 	<p class="lead">Login above:</p>
-	<p class="lead">Superuser: super@email.com (has all privileges)</p>
-	<p class="lead">Admin: admin@email.com</p>
-	<p class="lead">User: user@email.com</p>
+	<p class="lead">Superuser: super@email.com (has all privileges) | password: secret</p>
+	<p class="lead">Admin: admin@email.com | password: secret</p>
+	<p class="lead">User: user@email.com | password: secret</p>
 @stop

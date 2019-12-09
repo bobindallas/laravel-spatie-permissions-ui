9028fd3020929aeabfe9af2b6456980e9eb9aefa
bob <bob@sivercrowlabsl.com>
2019-12-09 10:42:31 -0600

    update login screen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,7 +57,7 @@
                                     {{ __('Login') }}
                                 </button>
 
-                                @if (Route::has('password.request') && 0)
+                                @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
